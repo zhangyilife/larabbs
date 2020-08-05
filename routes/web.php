34f558f0ec5d77ae7ab65205a8e716c227cd71e5
vagrant <vagrant@homestead>
2020-08-05 13:03:05 +0000

    修改css

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/','PagesController@root')->name('root');
+
+Auth::routes();
+
+//Route::get('/home', 'HomeController@index')->name('home');
