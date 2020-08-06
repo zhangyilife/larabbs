874c6e6b35a48899fd823630e71f837affff7fe2
vagrant <vagrant@homestead>
2020-08-06 09:15:55 +0000

    限制头像分辨率

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/','PagesController@root')->name('root');
 
 Auth::routes();
+Auth::routes(['verify' => true]);
 
 Route::resource('users','UsersController',['only'=>['show','update','edit']]);
 
