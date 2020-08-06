70c12a788f5cd8e86be76084ecaf3eb0c91f768e
vagrant <vagrant@homestead>
2020-08-06 09:30:14 +0000

    授权访问

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,12 +5,18 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Http\Requests\UserRequest;
+use auth;
 
 //use App\Handlers\ImageUploadHandler;
 use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth',['except'=>['show']]);
+    }
+
     public function show(User $user)
     {
     	return view('users.show',compact('user'));
@@ -18,11 +24,13 @@ class UsersController extends Controller
 
     public function edit(User $user)
     {
+        $this->authorize('update',$user);
     	return view('users.edit',compact('user'));
     }
 
     public function update(UserRequest $request,ImageUploadHandler $uploader,User $user)
     {
+        $this->authorize('update',$user);
         $data = $request->all();
 
         if ($request->avatar) {
