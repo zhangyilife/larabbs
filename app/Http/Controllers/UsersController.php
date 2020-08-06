53fbe30d77d34dc865d32a615a7d8b09a68afcb9
vagrant <vagrant@homestead>
2020-08-06 04:34:16 +0000

    修改配置

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\User;
 use App\Http\Requests\UserRequest;
 
+//use App\Handlers\ImageUploadHandler;
 use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
